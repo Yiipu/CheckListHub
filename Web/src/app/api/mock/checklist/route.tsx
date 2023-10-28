3a04f889516c8562b44aa492273bd98b2bc0559e
Yiipu <jaap20035@outlook.com>
2023-10-28 18:39:04 +0800

    mock sever

---
+++ Web/src/app/api/mock/checklist/route.tsx
@@ -0,0 +1,11 @@
+import { MockList } from "@/models/samples"
+import { NextRequest } from "next/server"
+import sleep from "../sleeper"
+
+export async function GET(request: NextRequest) {
+    await sleep(200)
+    const searchParams = request.nextUrl.searchParams
+    const query = searchParams.get('id')
+    MockList.id = query || ''
+    return Response.json(MockList)
+  }
---
+++ Web/src/app/api/mock/collection/route.tsx
@@ -0,0 +1,11 @@
+import { MockStarList } from "@/models/samples"
+import { NextRequest } from "next/server"
+import sleep from "../sleeper"
+
+export async function GET(request: NextRequest) {
+  await sleep(200)
+  const searchParams = request.nextUrl.searchParams
+  const query = searchParams.get('id')
+  MockStarList.id = query || ''
+  return Response.json(MockStarList)
+}
