2d4112a177a32ea90feb2cd0b997f58d98c4a615
Yiipu <jaap20035@outlook.com>
2023-11-10 17:24:57 +0800

    fix

--- Web/src/util/useStorage.tsx
+++ Web/src/util/useStorage.tsx
@@ -4,6 +4,7 @@ import { StateCheckList } from '@/context/StateCheckListProvider'
 import { useState, useEffect, useContext } from 'react'
 
 async function postProgress(progress: boolean[], stateCheckList: StateChecklist, session: GithubSession) {
+    console.log(JSON.stringify(progress))
     const res = await fetch(
         `${process.env.NEXT_PUBLIC_FE_URL}checklist/progress`,
         {
@@ -23,11 +24,13 @@ export default function useStorage(key: string, initialValue: string) {
     const session = useContext(Session)
     const stateCheckList = useContext(StateCheckList)
     const remoteProgress = stateCheckList.state.progress
+    console.log(stateCheckList.state.teamid)
+    const isTeam = stateCheckList.state.teamid !== '0'
     console.log(remoteProgress)
 
     const [storedValue, setStoredValue] = useState(() => {
         if (isBrowser) {
-            if (session) {
+            if (session && isTeam) {
                 if (!remoteProgress) {
                     return JSON.parse(initialValue)
                 }
@@ -36,6 +39,7 @@ export default function useStorage(key: string, initialValue: string) {
                 return remoteProgress.length ? remoteProgress : JSON.parse(initialValue)
             } else {
                 const item = localStorage.getItem(key)
+                console.log(item)
                 return item ? JSON.parse(item) : JSON.parse(initialValue)
             }
         }
@@ -43,13 +47,13 @@ export default function useStorage(key: string, initialValue: string) {
     })
 
     useEffect(() => {
-        if (session) {
+        if (session && isTeam) {
             postProgress(storedValue, stateCheckList, session)
-            console.log(`上传进度。。。${storedValue}`)
         } else {
-            localStorage.setItem(key, JSON.stringify([true, false]))
+            console.log(storedValue)
+            localStorage.setItem(key, JSON.stringify(storedValue))
         }
-    }, [key, session, stateCheckList, storedValue])
+    }, [isTeam, key, session, stateCheckList, storedValue])
 
     return [storedValue, setStoredValue]
 }
